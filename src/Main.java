129e74fc14032a6db55ecb4181086f6859cb8e6d
nazaninh0 <nazanin.heydari@learnet.se>
2024-01-21 22:44:58 +0100

    commit exampel

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("vinter projekt");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("namnge din tamagochi");
+        scan.nextLine();
+        String name;
+        name = scan.nextLine();
+        scan.nextLine();
+        tamagochi tam = new tamagochi(name);
+        tam.feed();
+        tam.teach("hej");
+        tam.Hi();
     }
+
 }
--- src/tamagochi.java
+++ src/tamagochi.java
@@ -2,12 +2,49 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class tamagochi {
+    private int hunger ;
     private int boredom=0;
     private ArrayList<String> words = new ArrayList<String>();
     private boolean isAlive=true;
     private Random generator =new Random();
     private String name;
-    public Tamagochi(String name){
+    public tamagochi(String name){
         this.name=name;
+    }
+    public void feed (){
+     hunger--;
+    }
+    public void Hi (){
+        System.out.println(generator);
+        ReduceBoredom();
+
+    }
+    public void teach ( String word ){
+        words.add(word);
+        ReduceBoredom();
+
+    }
+    public void Tick (){
+        hunger++;
+        boredom++;
+        if( hunger>10 || boredom > 10){
+            boolean isAlive =false ;
+        }
+        else {
+            boolean isAlive = true;
+        }
+    }
+    public void PrintStats(){
+        System.out.println("Hunger: " +hunger);
+        System.out.println(boredom);
+        System.out.println(isAlive);
+
+    }
+    public boolean GetAlive (){
+        return isAlive;
+    }
+    public void ReduceBoredom(){
+        boredom--;
+
     }
 }
